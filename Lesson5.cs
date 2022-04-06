cc1a2f74e9bfff8d553501b20637665cbb311471
Aleksandr Stepanov <alex_stepanoff@inbox.ru>
2022-04-06 23:39:33 +0300

    made homework p.1,2,3 p.1 & p.2 made as one In p.3 used modifided method from Lesson 4

---
+++ Lesson5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5_Homework_1
+{
+    internal static class Lesson5
+    {
+        internal static byte[] GetData(string info)
+        {
+            byte[] _data = new byte[info.Length];
+            string buffer = "";
+            int j = 0;
+            //make byte array with string length
+            for (int i = 0; i < info.Length; i++)
+            {
+                buffer += info[i];
+                if (info[i] == ' ' || i == info.Length - 1)
+                {
+                    _data[i] = byte.Parse(buffer);
+                    buffer = "";
+                    j++;
+                }
+            }
+            //make byte array without 0
+            byte[] data = new byte[j];
+            j = 0;
+            for (int i = 0; i < _data.Length; i++)
+            {
+                if (_data[i] != 0)
+                {
+                    data[j] = _data[i];
+                    j++;
+                }
+            }
+            return data;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,39 @@ namespace Lesson5_Homework_1
     {
         static void Main(string[] args)
         {
-            //start homework
+            Console.WriteLine("-------part1 + part2-------");
+            //Make file, append date and time, append file by lines.
+            Console.WriteLine("Press \"x\" to stop.\nEnter any text below:");
+            DateTime now = DateTime.Now;
+            string data = string.Empty;
+            string filename = "text1.txt";
+            FileInfo fileInfo = new FileInfo(filename);
+
+            File.AppendAllLines(filename, new[] {now.ToString()});
+
+            while (data != "x")
+            {
+                data = Console.ReadLine();
+                if (data != "x")
+                    File.AppendAllLines(filename, new[] {data});
+            }
+
+            Console.WriteLine("Path to file: " + fileInfo.FullName);
+
+            Console.WriteLine("\n-------part3-------");
+            Console.WriteLine("Enter numbers(0..255) separated with space:");
+            byte[] numbers = Lesson5.GetData(Console.ReadLine());
+
+            File.WriteAllBytes("numbers.bin", numbers);
+            fileInfo = new FileInfo("numbers.bin");
+            Console.WriteLine("Path to file: " + fileInfo.FullName);
+            byte[] fromFile = File.ReadAllBytes(fileInfo.FullName);
+
+            Console.WriteLine("Reading data from: " + fileInfo.FullName);
+            for (int i = 0; i < fromFile.Length; i++)
+                Console.Write(fromFile[i] + " ");
+
+            Console.ReadKey();
         }
     }
 }
